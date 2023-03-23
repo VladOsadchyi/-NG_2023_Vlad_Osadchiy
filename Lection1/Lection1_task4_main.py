63e5266de83e3f1b619f1a1b29d93ad519d8c4ab
VladOsadchyi <vladosadcyj340@gmail.come>
2023-03-23 13:51:45 +0200

    fixed bugs

--- Lection1/Lection1_task4_main.py
+++ Lection1/Lection1_task4_main.py
@@ -1,6 +1,12 @@
 fisrtnumber=float(input("Enter your a(first number): "))
 secondnumber=float(input("Enter your b(second number): "))
 thirdnumber=float(input("Enter your c(third number): "))
-resulta=print("First="+str(fisrtnumber**0.5))
-resultb=print("Second="+str(secondnumber**0.5))
-resultc=print("Third="+str(thirdnumber**0.5))
+Dis=secondnumber**2-(4*fisrtnumber*thirdnumber)
+print("Your D= "+str(Dis))
+if Dis>0:
+    firstx1=(-secondnumber+Dis**0.5)/(2*fisrtnumber)
+    secondx2=(-secondnumber-Dis**0.5)/(2*fisrtnumber)
+    print("x1= "+str(firstx1))
+    print("x2= "+str(secondx2))
+if Dis<0:
+    print("There are no D")
