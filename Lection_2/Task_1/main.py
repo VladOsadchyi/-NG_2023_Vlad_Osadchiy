ff88717c8c7440a1034f3c87b9375f781ea81951
VladOsadchyi <vladosadcyj340@gmail.come>
2023-03-30 14:06:02 +0300

    Fixud bugs

--- Lection_2/Task_1/main.py
+++ Lection_2/Task_1/main.py
@@ -1,10 +1,10 @@
+print("This program will count the number of your elements in yourlist")
+print("After this it will find element which you must write if you want to know the number of that element in your list")
 yourel=input("Enter your elements: ").split()
 countel=0
-counter=0
 for i in yourel:
     countel+=1
 print("All your elements: "+str(countel))
 yourchoice=input("Enter your choice: ")
-if yourchoice in yourel:
-    counter+=1
-print("How many el of your choice in yourlist"+str(counter))
+counter=yourel.count(yourchoice)
+print("How many el of your choice in yourlist: "+str(counter))
--- Lection_2/Task_2/main.py
+++ Lection_2/Task_2/main.py
@@ -1,2 +1,9 @@
-yourelements=input("Enter your elements: ").split(" ")
-print(set(yourelements))
+from collections import Counter
+print("This progran will return your unique elements")
+yourelements=input("Enter your elements: ").split()
+finallist=[]
+counterel=Counter(yourelements)
+for i, count in counterel.items():
+    if count==1:
+        finallist.append(i)
+print(finallist)
