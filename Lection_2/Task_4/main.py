6dbccb68c89c946dfcf8e72fb8aefe1b50cae51d
VladOsadchyi <vladosadcyj340@gmail.come>
2023-04-21 16:36:02 +0300

    added lection_2 task_4

--- Lection_2/Task_4/main.py
+++ Lection_2/Task_4/main.py
@@ -1,5 +1,7 @@
+import sys
 yourbooks=[]
 while True:
+    print(" 1-add book;\n 2-delete the book;\n 3-find book and change parameters(if you want)\n 4-finish this program")
     yourchoice=int(input("Enter your choice: "))
     match yourchoice:
         case 1:
@@ -16,7 +18,7 @@ while True:
                 "binding":binding
             }
             yourbooks.append(book)
-            print(yourbooks)
+            print(yourbooks, "\n")
         case 2:
             title=input("Enter the name of book which you want to delete: ")
             for book in yourbooks:
@@ -24,4 +26,42 @@ while True:
                     yourbooks.remove(book)
             print(yourbooks)
             break
+        case 3:
+            findbook=input("Enter the name of book which you want to find: ")
+            for book in yourbooks:
+                if book["title"]==findbook:
+                    print(book)
+                    print("Do you want to change something in it?Y/N")
+                    yourYN=input("")
+                    if yourYN=="Y":
+                        print(" 1-title;\n 2-author;\n 3-pages;\n 4-genre;\n 5-binding;")
+                        changedelofbook=int(input("Enter the parameter:"))
+                        match changedelofbook:
+                            case 1:
+                                newtitle=input("Enter your new title: ")
+                                book["title"]=newtitle
+                                print(book)
+                            case 2:
+                                newauthor=input("Enter your new author: ")
+                                book["author"]=newauthor
+                                print(book)
+                            case 3:
+                                newpages=input("Enter your new pages: ")
+                                book["pages"]=newpages
+                                print(book)
+                            case 4:
+                                newgenre=input("Enter your new genre: ")
+                                book["genre"]=newgenre
+                                print(book)
+                            case 5:
+                                newbinding=input("Enter your new binding: ")
+                                book["binding"]=newbinding
+                                print(book)
+                    else:
+                        print("Ok")
+                else:
+                    print("There is no such book")            
+        case 4:
+            sys.exit()
             
+                
